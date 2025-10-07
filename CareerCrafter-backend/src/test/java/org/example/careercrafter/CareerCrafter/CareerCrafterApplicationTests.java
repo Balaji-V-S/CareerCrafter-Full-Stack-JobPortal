e06de46acd6ca4c3b7b0a7e0f670c8037e61ac35
Balaji-V-S <vsbrobot@gmail.com>
2025-10-07 11:48:36 +0530

    feat: adding backend services for job portal app

---
+++ CareerCrafter-backend/src/test/java/org/example/careercrafter/CareerCrafter/CareerCrafterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.careercrafter.CareerCrafter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerCrafterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
